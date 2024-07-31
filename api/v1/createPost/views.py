b3f1067667306e79aad311fd730c05f317877bd8
Ayisha-SS <13aiysh246@gmail.com>
2024-07-31 17:45:11 +0530

    'update'

--- api/v1/createPost/views.py
+++ api/v1/createPost/views.py
@@ -26,8 +26,3 @@ def create_post(request):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-       
-    
-    
-    
