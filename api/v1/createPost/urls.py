1305ec22c2c7c321c685a82a6c0774a45bde8beb
Ayisha-SS <13aiysh246@gmail.com>
2024-08-05 01:53:55 +0530

    'update'

--- api/v1/createPost/urls.py
+++ api/v1/createPost/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from api.v1.createPost import views
 # from api.v1.posts.views import post
-from api.v1.createPost.views import PostDetailView
+from api.v1.createPost.views import PostDetailView, DeletePostView
 
 
 urlpatterns = [
     path('createpost/',views.create_post),
     path('view/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    path('createpost/<int:pk>/',  DeletePostView.as_view())
 ]
--- api/v1/createPost/views.py
+++ api/v1/createPost/views.py
@@ -3,12 +3,14 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
 from api.v1.createPost.serializers import CreateSerializer
 from posts.models import CreatePost
 from api.v1.createPost.serializers import ViewSerializer
  
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST','DELETE'])
 @permission_classes([IsAuthenticated])
 
 def create_post(request):
@@ -28,12 +30,28 @@ def create_post(request):
         else:
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
+
+class DeletePostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        try:
+            post = CreatePost.objects.get(pk=pk)
+        except CreatePost.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class PostDetailView(generics.RetrieveAPIView):
     queryset = CreatePost.objects.all()
     serializer_class = ViewSerializer
+
+
+
     
 
 
